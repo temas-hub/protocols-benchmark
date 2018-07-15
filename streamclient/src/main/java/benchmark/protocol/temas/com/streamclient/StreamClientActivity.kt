077049e2e5d47a97ff51df2ca5676b7d5e6ae380
Artem Zhdanov <artem.zhdanov@kohls.com>
2018-07-15 16:37:18 +0200

    - Added empty Android module - Optimized build configuration

---
+++ streamclient/src/main/java/benchmark/protocol/temas/com/streamclient/StreamClientActivity.kt
@@ -0,0 +1,12 @@
+package benchmark.protocol.temas.com.streamclient
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StreamClientActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stream_client)
+    }
+}
